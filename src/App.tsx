7ddce792d21a087aceb633ca30db2e00b890cf4e
irsyaddd <100660393+irsyaddd@users.noreply.github.com>
2023-01-27 12:46:56 +0700

    try

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p className="text-yellow-400">heeeyhoooo</p>
+        <p className="text-yellow-400">Hello World</p>
         <a
           className="App-link"
           href="https://reactjs.org"
