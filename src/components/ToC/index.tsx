466532aaa579e84b561a79f3c75849e21efd0c52
irsyaddd <100660393+irsyaddd@users.noreply.github.com>
2023-01-29 07:24:58 +0700

    remove unused import

--- src/components/ToC/index.tsx
+++ src/components/ToC/index.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useState } from "react";
 import { Images } from "../../assets";
 import { TASK_LIST } from "../../data";
 
